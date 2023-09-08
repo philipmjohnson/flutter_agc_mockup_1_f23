3ad54ef9f7afbfcace83353fb815f777ae4467d5
Philip Johnson <johnson@hawaii.edu>
2023-09-07 14:56:21 -1000

    checkpoint

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,9 +1,16 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 
-import 'sample_feature/sample_item_details_view.dart';
-import 'sample_feature/sample_item_list_view.dart';
+import 'pages/chapters/chapters_view.dart';
+import 'pages/discussions/discussions_view.dart';
+import 'pages/gardens/gardens_view.dart';
+import 'pages/help/help_view.dart';
+import 'pages/home/home_view.dart';
+import 'pages/members/members_view.dart';
+import 'pages/outcomes/outcomes_view.dart';
+import 'pages/page_not_found/page_not_found_view.dart';
+import 'pages/seeds/seeds_view.dart';
+import 'pages/signin/signin_view.dart';
+import 'pages/signup/signup_view.dart';
 import 'settings/settings_controller.dart';
 import 'settings/settings_view.dart';
 
@@ -20,42 +27,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     // Glue the SettingsController to the MaterialApp.
     //
-    // The AnimatedBuilder Widget listens to the SettingsController for changes.
+    // The ListenableBuilder Widget listens to the SettingsController for changes.
     // Whenever the user updates their settings, the MaterialApp is rebuilt.
-    return AnimatedBuilder(
-      animation: settingsController,
+    return ListenableBuilder(
+      listenable: settingsController,
       builder: (BuildContext context, Widget? child) {
         return MaterialApp(
-          // Providing a restorationScopeId allows the Navigator built by the
-          // MaterialApp to restore the navigation stack when a user leaves and
-          // returns to the app after it has been killed while running in the
-          // background.
-          restorationScopeId: 'app',
-
-          // Provide the generated AppLocalizations to the MaterialApp. This
-          // allows descendant Widgets to display the correct translations
-          // depending on the user's locale.
-          localizationsDelegates: const [
-            AppLocalizations.delegate,
-            GlobalMaterialLocalizations.delegate,
-            GlobalWidgetsLocalizations.delegate,
-            GlobalCupertinoLocalizations.delegate,
-          ],
-          supportedLocales: const [
-            Locale('en', ''), // English, no country code
-          ],
-
-          // Use AppLocalizations to configure the correct application title
-          // depending on the user's locale.
-          //
-          // The appTitle is defined in .arb files found in the localization
-          // directory.
-          onGenerateTitle: (BuildContext context) =>
-              AppLocalizations.of(context)!.appTitle,
-
-          // Define a light and dark color theme. Then, read the user's
-          // preferred ThemeMode (light, dark, or system default) from the
-          // SettingsController to display the correct theme.
           theme: ThemeData(),
           darkTheme: ThemeData.dark(),
           themeMode: settingsController.themeMode,
@@ -67,13 +44,30 @@ class MyApp extends StatelessWidget {
               settings: routeSettings,
               builder: (BuildContext context) {
                 switch (routeSettings.name) {
+                  case ChaptersView.routeName:
+                    return const ChaptersView();
+                  case DiscussionsView.routeName:
+                    return const DiscussionsView();
+                  case GardensView.routeName:
+                    return const GardensView();
+                  case HelpView.routeName:
+                    return const HelpView();
+                  case HomeView.routeName:
+                    return HomeView();
+                  case MembersView.routeName:
+                    return const MembersView();
+                  case OutcomesView.routeName:
+                    return const OutcomesView();
+                  case SeedsView.routeName:
+                    return const SeedsView();
                   case SettingsView.routeName:
                     return SettingsView(controller: settingsController);
-                  case SampleItemDetailsView.routeName:
-                    return const SampleItemDetailsView();
-                  case SampleItemListView.routeName:
+                  case SigninView.routeName:
+                    return const SigninView();
+                  case SignupView.routeName:
+                    return const SignupView();
                   default:
-                    return const SampleItemListView();
+                    return const PageNotFoundView();
                 }
               },
             );
